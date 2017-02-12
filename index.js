f9c968af36a44afa3e69489c15c0a4c6714a5658
Leandra <lli5ba@virginia.edu>
2017-02-12 12:44:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/webhooks', function (req, res) {
   if (req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
     res.send(req.query['hub.challenge'])
   }
-  res.send('Error, wrong token')
+  res.sendStatus(400);
 })
 
 // to send messages to facebook
