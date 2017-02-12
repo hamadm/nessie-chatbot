e10455d422648e04edd41913f0334c5be3f53d8d
Leandra <lli5ba@virginia.edu>
2017-02-12 10:55:14 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const WIT_TOKEN = 'OTQRF4JLPHHMLY2TDY7GFMTYFMTMP2CR'
-      //process.env.WIT_TOKEN
+const WIT_TOKEN = process.env.WIT_TOKEN
 if (!WIT_TOKEN) {
   throw new Error('Missing WIT_TOKEN. Go to https://wit.ai/docs/quickstart to get one.')
 }
 
 
-var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAQ8XHBqm7IBAAxVJhrClZAChzu6YZBiOKmPlWLI6K6sHJCRoYd6wZADL8ZBQDDmnEytm2be3v6IJ3bPF5dTP5HrqxQgi0lFMTYWof89B7tZCKZB24WLnJWeIWwSXXQM1mDNWr6hMCuWznbjFOeXYJN2PMn0YLPqZCoKqH4ZBeSR6AZDZD';
+var FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN
 if (!FB_PAGE_TOKEN) {
 	throw new Error('Missing FB_PAGE_TOKEN. Go to https://developers.facebook.com/docs/pages/access-tokens to get one.')
 }
